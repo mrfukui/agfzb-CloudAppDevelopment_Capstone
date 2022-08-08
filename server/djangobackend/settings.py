a98e0adaaac274d368723b4b3030f5191b69cbe9
mrfukui <mr.fukui@yahoo.com>
2022-08-07 20:12:56 -0700

    update allowed hosts

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["djangoappFukui2.mybluemix.net", "djangoappFukui2.mybluemix.net/static","localhost"]
 
 
 # Application definition
