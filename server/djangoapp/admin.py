6b1e8b0ca6cc91e89d1a77c1ba53e9950d3e509e
mrfukui <mr.fukui@yahoo.com>
2022-08-18 02:50:48 +0000

    added review view etc

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # from .models import related models
-from .models import *
+from .models import CarMake, CarModel
 
 # Register your models here.
 
@@ -20,4 +20,4 @@ class CarMakeAdmin(admin.ModelAdmin):
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
-admin.site.reigster(CarModel, CarModelAdmin)
+admin.site.register(CarModel, CarModelAdmin)
